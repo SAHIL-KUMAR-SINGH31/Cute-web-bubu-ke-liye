8ac131934b3a93f7a4466b92a25dc6a5333d7447
SAHIL-KUMAR-SINGH31 <ss636998@gmail.com>
2025-06-15 08:12:57 +0530

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./pages/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        cursive: ['"Great Vibes"', 'cursive'],
+      },
+    },
+  },
+  plugins: [],
+}
